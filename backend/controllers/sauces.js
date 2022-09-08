03a3c20c219531b6931f24c5d41f7f3d83073ef4
frederick486 <frederick.gaittet@orange.fr>
2022-09-08 16:12:18 +0200

    Simplification fonction modifySauce

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -40,20 +40,14 @@ exports.modifySauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id }).then((sauce) => {
     const filename = sauce.imageUrl.split("/images/")[1];
     fs.unlink(`images/${filename}`, () => {
-      const sauceObject = req.file
-        ? {
+      const sauceObject = req.file ? 
+          {
             ...JSON.parse(req.body.sauce),
-            imageUrl: `${req.protocol}://${req.get("host")}/images/${
-              req.file.filename
-            }`,
-          }
-        : { ...req.body };
-      Sauce.updateOne(
-        { _id: req.params.id },
-        { ...sauceObject, _id: req.params.id }
-      )
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${ req.file.filename }`,
+          } : { ...req.body };
+      Sauce.updateOne( { _id: req.params.id }, { ...sauceObject, _id: req.params.id } )
         .then(() => res.status(200).json({ message: "Sauce modifiée !" }))
-        .catch((error) => res.status(400).json({ error }));
+        .catch(error => res.status(400).json({ error }));
     });
   });
 };
